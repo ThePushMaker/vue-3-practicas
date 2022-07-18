f3b549dea6d933d5d36600da97422418fee47fee
Martín Samuel Calderón Ojeda <martincalderon@iMac-de-Martin.local>
2022-07-18 11:51:04 -0600

    18

---
+++ p18 -multiples componentes/main.js
@@ -0,0 +1,10 @@
+const app={
+    data(){
+        return{
+            usuario:'codeStack',
+            correo:'',
+            clave:'',
+        }
+    }
+}
+const _app=Vue.createApp(app);
---
+++ p18 -multiples componentes/menu.component.js
@@ -0,0 +1,22 @@
+_app.component('menu-component',{
+    template:`<nav class="indigo darken-4">
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">{{usuario}}</a></li>
+                    </ul>
+                </div>
+                </nav>
+                <input type="text" v-model="usuario">`,
+                //las props son de solo lectura, estas no se pueden modificar porqué se envían via parametro, son de solo lectura    
+          props:{
+            usuario: String
+          },
+          data(){
+            return{
+                miUsuario:this.usuario
+            }
+          }
+})
