c1298f5a8372f83a202e4da0c9fe29db514aed42
Martín Samuel Calderón Ojeda <martincalderon@iMac-de-Martin.local>
2022-07-20 12:17:30 -0600

    20

---
+++ p21--hola-mundo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld v-bind:msg="mensaje" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      mensaje:"Hola mundo, otra vez..."
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ p22/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+<!--el scoped significa que estos atributos no se van a poder heredar a otros componentes, ni tampoco se va a poder aplicar dentro de este componente, no se va a poder aplicar a los demás-->
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
